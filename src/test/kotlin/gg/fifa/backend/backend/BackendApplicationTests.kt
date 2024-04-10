bb0a334407ed26569fc8de96b872d58e1622e4e3
김원현 <hermes@gim-wonhyeons-MacBook-Pro.local>
2024-04-11 00:16:24 +0900

    Initial Commit

---
+++ src/test/kotlin/gg/fifa/backend/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package gg.fifa.backend.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
